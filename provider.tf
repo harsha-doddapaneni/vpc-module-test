1c4a117ec1a7a9f4b6e064533d686903b6305890
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-05-02 12:36:52 +0530

    vpc

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
     backend "s3" {
-        bucket = "81s-remot-state"
+        bucket = "81s-remot-stat"
         key = "expense-vpc"
         region = "us-east-1"
         dynamodb_table = "81s-locking"
